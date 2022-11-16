95311513d22dc2d44f4de1b9f58df4a2a441e1a1
abinasureskumar <abina30.08.2003@gmail.com>
2022-11-16 10:43:09 +0000

    add Test

--- src/test/java/ch/zli/m223/EntryResourceTest.java
+++ src/test/java/ch/zli/m223/EntryResourceTest.java
@@ -1,10 +1,20 @@
 package ch.zli.m223;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.Test;
+
+import ch.zli.m223.model.Entry;
+
+//import com.google.errorprone.annotations.Var;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import java.time.LocalDateTime;
+
+//import javax.swing.text.AbstractDocument.Content;
 
 @QuarkusTest
 public class EntryResourceTest {
@@ -16,6 +26,31 @@ public class EntryResourceTest {
           .then()
              .statusCode(200)
              .body(is("[]"));
+    }
+
+    @Test
+    public void testDeleteEndpoint() {
+        var entry = new Entry();
+        entry.setCheckIn(LocalDateTime.now());
+        entry.setCheckOut(LocalDateTime.now());
+
+        given().contentType(ContentType.JSON).body(entry).post("/entries");
+        given().when().delete("/entries/1").then().statusCode(204);
+    }
+
+    @Test
+    public void testUpdate() {
+        var entry = new Entry();
+        LocalDateTime now = LocalDateTime.now();
+        entry.setCheckIn(now);
+        entry.setCheckOut(now);
+
+        given().contentType(ContentType.JSON).body(entry).post("/entries");
+        var newEntry = new Entry();
+        newEntry.setCheckIn(LocalDateTime.now());
+        newEntry.setCheckOut(LocalDateTime.now());
+
+        given().when().contentType(ContentType.JSON).body(newEntry).put("/entries").then().statusCode(200);
     }
 
 }
